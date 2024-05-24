9f13f34bed9f4c61a3280de55b894d1525075b49
codydbarr <cdb9657@nyu.edu>
2024-05-24 16:45:50 -0400

    test submission

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,7 +6,7 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer = "No"
+        answer = "pcap"
         
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
